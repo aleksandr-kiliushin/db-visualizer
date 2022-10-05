25e24d34c5a6b4bfd1cdab2dc01886ec6fc5fb08
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-05 22:59:40 +0400

    chore: install @types/pg

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
+import { Client } from "pg"
+
 require("dotenv").config({ path: "./local.env" })
 
 const bootstrap = async () => {
   console.log("process.env.PORT >>", process.env.PORT)
+  const client = new Client()
+  await client.connect()
+  const res = await client.query("SELECT $1::text as message", ["Hello world!"])
+  console.log(res.rows[0].message) // Hello world!
+  await client.end()
 }
 
 bootstrap()
