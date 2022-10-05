2d6caf6d44f7447987459173edad249428c9c01b
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-05 22:52:12 +0400

    chore: Uninstall typeorm

--- src/config/ormConfig.ts
+++ src/config/ormConfig.ts
@@ -1,17 +1,17 @@
-import { DataSource, DataSourceOptions } from "typeorm"
+// import { DataSource, DataSourceOptions } from "typeorm"
 
-export const ormConfig: DataSourceOptions = {
-  database: process.env.DATABASE_NAME ?? "personal_app_db",
-  entities: ["dist/**/*.entity.js"],
-  host: process.env.DATABASE_HOST ?? "localhost", // Name of database service in compose.dev.yml.
-  migrations: ["dist/migrations/*.js"],
-  migrationsTableName: "migrations",
-  password: process.env.POSTGRES_PASSWORD ?? "123",
-  port: 5432,
-  ssl: false,
-  synchronize: false, // process.env.MODE === "dev", // Once you get into production you'll need to synchronize model changes into the database. It is unsafe to use `synchronize: true` for schema synchronization on production once you get data in your database. Here is where migrations come to help.
-  type: "postgres",
-  username: process.env.DATABASE_USERNAME ?? "postgres",
-}
+// export const ormConfig: DataSourceOptions = {
+//   database: process.env.DATABASE_NAME ?? "personal_app_db",
+//   entities: ["dist/**/*.entity.js"],
+//   host: process.env.DATABASE_HOST ?? "localhost", // Name of database service in compose.dev.yml.
+//   migrations: ["dist/migrations/*.js"],
+//   migrationsTableName: "migrations",
+//   password: process.env.POSTGRES_PASSWORD ?? "123",
+//   port: 5432,
+//   ssl: false,
+//   synchronize: false, // process.env.MODE === "dev", // Once you get into production you'll need to synchronize model changes into the database. It is unsafe to use `synchronize: true` for schema synchronization on production once you get data in your database. Here is where migrations come to help.
+//   type: "postgres",
+//   username: process.env.DATABASE_USERNAME ?? "postgres",
+// }
 
-export const dataSource = new DataSource(ormConfig)
+// export const dataSource = new DataSource(ormConfig)
