5d89d5a5a1236f562b102019750c2822af0acb7c
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-06 21:13:46 +0400

    refactor

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -1,8 +1,7 @@
+import { execute } from "../src/index"
 import { parseDbmlResult } from "./parseDbmlResult"
 
 const path = require("path")
-
-const { execute } = require("../src/index")
 
 describe("execute", () => {
   const dumpPath = path.join(__dirname, "..", "sample-files", "dump.sql")
